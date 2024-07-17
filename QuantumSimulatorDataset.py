26007f8d51b5e2db156547d9e3eaf6d9ba1e9fdc
Sonny Lowe <sonny@princeton.edu>
2024-07-17 14:26:22 -0700

    Entropy and MI Addition

--- QuantumSimulatorDataset.py
+++ QuantumSimulatorDataset.py
@@ -199,7 +199,7 @@ if __name__ == '__main__':
     # (0,1),(2,3),(4,5),(6,7), (8,9), (10,11), (1,2),(3,4),(5,6),(7,8),(9,10), (11,0),
     # (0,1),(2,3),(4,5),(6,7), (8,9), (10,11), (1,2),(3,4),(5,6),(7,8),(9,10), (11,0)]
 
-    # save_circuit_params(params, 'dense_small.param')
+    save_circuit_params(params, 'dense_small.param')
 
     params = generate_circuit_params(12, 12)
 
